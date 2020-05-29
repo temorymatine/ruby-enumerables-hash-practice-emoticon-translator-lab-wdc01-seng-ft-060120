afb6c369a3fbb0ff812a1ed5a9cb582b903b92a3
Matine temory <temorymatine@gmail.com>
2020-05-29 02:56:22 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -28,14 +28,32 @@ else
   answer
 end
  
-
-  
-  
  
 end
 
 def get_japanese_emoticon(emogies_library,emogi)
 
- library = load_library(emogies_library)
+answer = ""
+
+ library = load_library(emogies_library) 
+  
+    library.each{|emoginame, innerhash|
+      
+       innerhash.each{|language,emote| 
+            
+            if emote == emogi 
+              answer = 
+       
+       
+       }
+                 }
+         
+ if answer == ""
+   answer = "Sorry, that emoticon was not found"
+   
+else
+  answer
+end
+ 
           
 end
