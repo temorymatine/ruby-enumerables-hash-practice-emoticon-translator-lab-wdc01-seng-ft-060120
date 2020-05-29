dc8587d3d3e005856055ea4333c7bc7d691b439c
Matine temory <temorymatine@gmail.com>
2020-05-29 02:45:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,35 @@
-# require modules here
+require "yaml"
+require 'pry'
 
-def load_library
-  # code goes here
+
+def load_library(emogies_library)
+  emogies_library = YAML.load_file('./lib/emoticons.yml')
+   emogies_library
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_english_meaning(emogies_library, emogi)
+  
+  answer = ""
+
+ library = load_library(emogies_library) 
+  
+    library.each{|emoginame, innerhash|
+      
+        if innerhash.value?(emogi)
+       answer =  emoginame
+        end
+        
+    }
+         
+  answer
+
+  
+  
+ 
 end
 
-def get_english_meaning
-  # code goes here
+def get_japanese_emoticon(emogies_library,emogi)
+
+ library = load_library(emogies_library)
+          
 end
